2ced27566195169ec2fd295620fa3170730200ae
Ubuntu <ubuntu@new-controller1.maas>
2021-11-03 11:45:18 +1030

    Added buildall script

---
+++ buildall.sh
@@ -0,0 +1,4 @@
+#docker build ocata-docker -t upgrader-ocata
+docker build queens-docker -t upgrader-queens
+docker build stein-docker -t upgrader-stein
+docker build ussuri-docker -t upgrader-ussuri
